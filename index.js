b06f80d8309a890f13cf4f1ac245fda558b37056
victor <victor.odhi@gmail.com>
2023-12-18 22:48:12 +0300

    completed test

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+
+const record = [
+    { year: "2015", result: "W" },
+    { year: "2014", result: "N/A" },
+    { year: "2013", result: "L" },
+];
+  
+function superbowlWin(record) {
+    const win = record.find(game => game.result === "W");
+  
+    if (win) {
+      return win.year;
+    } else {
+      return undefined;
+    }
+}
+
+const winYear = superbowlWin(record);
+if (winYear) {
+    console.log(`The Denver Broncos won the Superbowl in ${winYear}.`);
+} else {
+    console.log("The Denver Broncos have never won the Superbowl.");
+}
